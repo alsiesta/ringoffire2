d8883acc14310324a932f65bb367cad17e3014d4
alsiesta <as@daslizenzatelier.de>
2023-03-22 10:01:15 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-f935d',
+    appId: '1:676232804386:web:5b13ecc185b6b8c71bf27b',
+    storageBucket: 'ring-of-fire-f935d.appspot.com',
+    apiKey: 'AIzaSyDorutOQp8rXlnP7Y0JCRZ7DBNHcYPe2N0',
+    authDomain: 'ring-of-fire-f935d.firebaseapp.com',
+    messagingSenderId: '676232804386',
+  },};
